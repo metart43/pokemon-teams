91b0829fec1dfb59576fc86d40b0056cea1002ee
github username <metelskyi88@gmail.com>
2019-02-27 11:58:06 -0500

    almost everything working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 //solution goes here
 document.addEventListener("DOMContentLoaded",function (){
 fetchTrainers()
-document.querySelector('button').addEventListener('click', addingPokemon)
 })
 
 
@@ -22,9 +21,11 @@ function render(trainer) {
   const addBttn = document.createElement('button')
   const text = document.createTextNode('Add')
   addBttn.appendChild(text)
+  addBttn.id = trainer.id
+  addBttn.className = `button-${trainer.id}`
   trainerCard.append(trainerName, addBttn)
   mainHTML.appendChild(trainerCard)
-  document.querySelector('button').addEventListener('click', addingPokemon)
+  document.querySelector(`.button-${trainer.id}`).addEventListener('click', addingPokemon)
   const pokemonNames = trainer.pokemons.filter(pokemon => pokemon.nickname)
   pokemonNames.forEach(pokemon => renderPokemon(pokemon))
 }
@@ -33,18 +34,41 @@ function renderPokemon(pokemon){
   const pokemonList = document.createElement('ul')
   const pokemonEl = document.createElement('li')
   const releaseBttn = document.createElement('button')
-  releaseBttn.className = 'release'
+  releaseBttn.className = `release`
   releaseBttn.id = pokemon.id
   releaseBttn.innerText = 'release'
   pokemonEl.innerHTML = pokemon.nickname
   pokemonEl.id = pokemon.id
   pokemonEl.append(releaseBttn)
   pokemonList.appendChild(pokemonEl)
-  debugger
   const trainerCard = document.querySelector(`#card-${pokemon.trainer_id}`)
   trainerCard.appendChild(pokemonList)
+  document.getElementById(`${pokemon.id}`).addEventListener('click', releasePokemon)
 }
 
-function addingPokemon(){
+function addingPokemon(event){
+let data = {
+  trainer_id: event.target.id
+}
 console.log('inside');
+debugger
+fetch(`http://localhost:3000/pokemons`, {
+  method: "POST" ,
+  headers: {'Content-Type': 'application/json'},
+  body:JSON.stringify(data),
+}).then(response => response.json())
+.then(newPoke => renderPokemon(newPoke))
+}
+
+function releasePokemon(){
+  pokemonId = event.target.id
+  console.log('inside');
+  fetch(`http://localhost:3000/pokemons/${pokemonId}`, {
+    method: "DELETE"
+  })
+  .then((response) => {
+    debugger
+    document.getElementById(`${pokemonId}`).remove()
+  })
+
 }
